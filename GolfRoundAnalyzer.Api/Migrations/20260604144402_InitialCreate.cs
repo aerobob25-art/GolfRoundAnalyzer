b2b1236a8fea61683b7706b3d59057e0c1033de1
Lucas <lucas.christianson01@gmail.com>
2026-06-04 14:35:16 -0400

    Added initial DB migrations and fixed incorrect db connection

---
+++ GolfRoundAnalyzer.Api/Migrations/20260604144402_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GolfRoundAnalyzer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GolfRounds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DatePlayed = table.Column<string>(type: "text", nullable: false),
+                    CourseName = table.Column<string>(type: "text", nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    FairwaysHit = table.Column<int>(type: "integer", nullable: false),
+                    FairwaysPossible = table.Column<int>(type: "integer", nullable: false),
+                    GreensInRegulation = table.Column<int>(type: "integer", nullable: false),
+                    Putts = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GolfRounds", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GolfRounds");
+        }
+    }
+}
---
+++ GolfRoundAnalyzer.Api/Migrations/20260604145504_CreateGolfRounds.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GolfRoundAnalyzer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGolfRounds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
